2053e3b59cb2a7d730f77593446446874310498c
jconeff <jessicaconeff@gmail.com>
2020-11-23 14:58:22 -0500

    city name shows in current weatehr when searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,14 @@ cityform.addEventListener('submit', (event)=> {
     event.preventDefault();
     let uviIndex = document.querySelector('.uvi-index');
     let currentTemp = document.querySelector('.currenttempdegree');
-    let locationTimezone = document.querySelector('.location-timezone');
+    let locationTimezone = document.querySelector('.cityname');
     let city = document.querySelector('#cityname').value;
     let currentHum = document.querySelector('.currenthumidity');
     let currentWind = document.querySelector('.wind');
     let currentday = document.querySelector('.currentday');
-    let currentIcon = document.querySelector('.currentpic')
-    //let icon1 = document.querySelector('.icon1');
+    let currentIcon = document.querySelector('.currentpic');
+    
+    
     let date1 = document.querySelector('.date1');
     let date2 = document.querySelector('.date2');
     let date3 = document.querySelector('.date3');
@@ -23,9 +24,6 @@ cityform.addEventListener('submit', (event)=> {
     
 
     
-
-
-    
     
 //hide 5 day forecast until onclick of submit
 
@@ -34,16 +32,33 @@ cityform.addEventListener('submit', (event)=> {
    
 //API URL  current weather
            const currentApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c962694451f88c2f69a40aac9d384464&units=imperial';
+
+           
+           
            fetch(currentApi)
+        
            .then(response =>{
+
+           
+            
               return response.json();
+
+                    
+              
            })
 
+           
            .then(data =>{
-//API for lon and lat / five day weather
+
+            //name of city
+            locationTimezone.textContent = data.name;
+        //API for lon and lat / five day weather
                const apiOneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +data.coord.lat + '&lon=' + data.coord.lon +'&appid=c962694451f88c2f69a40aac9d384464&units=imperial';
+               
            fetch(apiOneCall)
            .then(response =>{
+
+
               return response.json();
            })
 
@@ -56,12 +71,10 @@ cityform.addEventListener('submit', (event)=> {
   let humLoop = document.querySelector('.humidity'+ index);
   let iconLoop = document.querySelector('.icon'+ index);
  
-  
-  
- 
+ //five day forecast info
   tempDegreeLoop.textContent = Math.round(oneCallData.daily[index].temp.day) + "F°"
   humLoop.textContent = 'Humidity:' + oneCallData.daily[index].humidity + '%'
-
+//five day forecast info
    let weatherPic1 = oneCallData.daily[index].weather[0].icon;
     iconLoop.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic1 + "@2x.png");
 
@@ -70,35 +83,35 @@ cityform.addEventListener('submit', (event)=> {
   
   
 }
+
+
            
 //end of loop
                
                const{temp, humidity, uvi, wind_speed} = oneCallData.current;
+               
+            
+           
             
              let weatherPic = oneCallData.current.weather[0].icon;
              currentIcon.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
 
-             //let weatherPic1 = oneCallData.daily[0].weather[0].icon;
-            // icon1.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic1 + "@2x.png");
+        
              
-
              
-
-
-
     
-            console.log(oneCallData);
+    
                
                //Set DOM Elements from API
                //current DOM Elements from API 
                currentTemp.textContent = 'Temperature:' + Math.round(temp) + "F°";
                currentHum.textContent = 'Humidity:' + humidity + '%';
                currentWind.textContent = 'Wind speed:' + wind_speed + 'MPH'; 
-               //currentIcon.src = icon;
-              
+               uviIndex.textContent = "UV Index:" + uvi;
+               //locationTimezone.textContent = oneCallData.name
                
-              
-             
+               
+        
 
                //current day + Next 5 days
             
@@ -109,10 +122,7 @@ cityform.addEventListener('submit', (event)=> {
                date4.textContent = moment().add(4,'days').format('LL'); 
                date5.textContent = moment().add(5,'days').format('LL'); 
                
-        
-               //Five day DOM Elements from API
-               hum.textContent = "Humidity:" + humidity; 
-               uviIndex.textContent = "UV Index:" + uvi;
+    
                
            });
 
